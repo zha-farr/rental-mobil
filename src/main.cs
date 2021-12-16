b7d2e9ce5605fd20f7f3bc0056a18e0d99f02ea7
Muhammad Zhafar Al Fathi <95948333+zha-farr@users.noreply.github.com>
2021-12-16 18:08:16 +0700

    Add files via upload

---
+++ src/main.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace RentalMobil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    internal class Mobil
+    {
+        public string Model { get; set; }
+        public string Warna { get; set; }
+        public int HargaPerHari { get; set; }
+        public string LicensePlate { get; set; }
+        public int FuelCapacity { get; set; }
+        public int KilometerUsed { get; set; }
+        public int SeatCapacity { get; set; }
+        public bool AirConditionerEnabled { get; set; }
+        /// <summary>
+        /// In some country also called CC
+        /// </summary>
+        public int EngineDisplacement { get; set; }
+        public string ColorPriority { get; set; }
+        public bool IsAutoMobile { get; set; }
+        public bool IsUsingPetrol { get; set; }
+
+        public Mobil(string model,
+                    int harga,
+                    int fuelCap,
+                    int seatCap,
+                    bool ac,
+                    int cc,
+                    string color,
+                    bool isMatic,
+                    bool isPetrol)
+        {
+            Model = model;
+            HargaPerHari = harga;
+            FuelCapacity = fuelCap;
+            SeatCapacity = seatCap;
+            AirConditionerEnabled = ac;
+            EngineDisplacement = cc;
+            ColorPriority = color;
+            IsAutoMobile = isMatic;
+            IsUsingPetrol = isPetrol;
+        }
+    }
+}
